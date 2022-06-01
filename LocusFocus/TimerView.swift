d3be03b1b13597641e3c81e528b70301665a5668
Jake Tamayo <hellojtmatthew@icloud.com>
2022-06-01 18:19:36 -0500

    TimerView for Locus Focus

---
+++ LocusFocus/TimerView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerView.swift
+//  LocusFocus
+//
+//  Created by Jacob Tamayo on 6/1/22.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
