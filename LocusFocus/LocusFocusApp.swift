03a4db24b32599e200c02a87973a3d84f2f82016
Jake Tamayo <hellojtmatthew@icloud.com>
2022-06-01 18:18:11 -0500

    Initial Commit

---
+++ LocusFocus/LocusFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocusFocusApp.swift
+//  LocusFocus
+//
+//  Created by Jacob Tamayo on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LocusFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
